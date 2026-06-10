2d0158d11fb2cb1452f90036486e7d3aa1c1064e
Daniel Urena <durena@bluecorela.com>
2026-06-10 10:18:51 -0500

    feat: Creacion del componente gds header

--- projects/gds-lib/src/lib/components/gds-header/gds-header.ts
+++ projects/gds-lib/src/lib/components/gds-header/gds-header.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
+type TypeHeader = 'NORMAL' | 'STEPPER' | 'NUMBERS';
 @Component({
   selector: 'lib-gds-header',
   templateUrl: './gds-header.html',
   styleUrl: './gds-header.scss',
   standalone: false
 })
-export class GdsHeader {}
+export class GdsHeader {
+  @Input() type: TypeHeader = 'NORMAL';
+  @Input() steppers: string[] = [];
+  @Output() back = new EventEmitter<boolean>();
+  @Output() exit = new EventEmitter<boolean>();
+  @ViewChild('stepper') set stepperContainer(element: ElementRef<HTMLDivElement> | undefined) {
+    if (element) {
+      console.log('STEPPER');
+    }
+  }
+  @ViewChild('number') set numberContainer(element: ElementRef<HTMLDivElement> | undefined) {
+    if (element) {
+      console.log('number');
+    }
+  }
+
+  constructor(
+    domSanitizer: DomSanitizer,
+    matIconRegistry: MatIconRegistry
+  ) {
+    matIconRegistry
+      .addSvgIcon('cerrar', domSanitizer.bypassSecurityTrustResourceUrl(`assets/cerrar.svg`))
+      .addSvgIcon('atras', domSanitizer.bypassSecurityTrustResourceUrl(`assets/atras.svg`))
+      .addSvgIcon('logo-sgb', domSanitizer.bypassSecurityTrustResourceUrl(`assets/logo-sgb.svg`));
+  }
+
+  public onBack(): void {
+    this.back.emit();
+  }
+
+  public onExit(): void {
+    this.exit.emit();
+  }
+
+}
